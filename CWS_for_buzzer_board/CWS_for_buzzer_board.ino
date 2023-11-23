bc13dbc30db8f543582286ab71c57952120f3f8b
SeungJiRyu <behumble930@gmail.com>
2023-11-23 13:48:43 +0900

    20231123

---
+++ CWS_for_buzzer_board/CWS_for_buzzer_board.ino
@@ -0,0 +1,60 @@
+/* Buzzer code */
+#define BUZZER 3 // buzzer output - PD3
+#define frequency 440.00; //라 음계, 사람이 가장 잘 인지하는 주파수
+volatile uint8_t DUTY = 1; // 부저음 크기(PWM) / [5, 15, 30, 95] 4단계로 분류
+
+//1차 경보 : 위험 감지 시(With partial braking), 띠 띠 띠 띠
+void buzzer_sound_mode1(uint8_t DUTY){
+  OCR2A = F_CPU / 256 / frequency -1; //make ra
+  OCR2B = OCR2A *DUTY/100; //for dB, 이 값에 비례해서 데시벨 결정
+  //pinMode(3,HIGH);
+  DDRD |= (1<<BUZZER);//on
+
+  delay(40);
+  DDRD &= ~(1<<BUZZER);//off
+  delay(70);
+}
+
+//2차 경보 : 충돌 예상 시(close at full braking), 띠----
+void buzzer_sound_mode2(uint8_t DUTY){
+  OCR2A = F_CPU / 256 / frequency -1;
+  OCR2B = OCR2A *DUTY/100;
+  //pinMode(3,HIGH);
+  DDRD |= (1<<BUZZER);
+  OCR2B = OCR2A *DUTY/100 ;
+
+  //Delay 10 * 2ms = 20ms, 20ms 동안 경보
+  delay(2000);
+}
+
+void setup() {
+  Serial.begin(57600);
+  pinMode(BUZZER,OUTPUT);
+  pinMode(6,INPUT);
+  pinMode(7,INPUT);
+
+  //Timercounter setting
+  TCCR2A |= (1<<COM2B1);
+  TCCR2A |= (1<<WGM21) |(1<<WGM20);
+  TCCR2B |= (1<<WGM22); // Fast PWM, TOP을 OCRA로 지정하였으므로, OCR2A의 값을 계산한다.
+  TCCR2B |= (1<<CS22) | (1<<CS21) | (0<<CS20);
+
+  OCR2A = F_CPU / 256 / frequency -1;
+  OCR2B = OCR2A *DUTY/100;
+}
+
+void loop() {
+
+  Serial.print(digitalRead(6));
+  Serial.print(",");
+  Serial.print(digitalRead(7));
+  Serial.println();
+
+  if((digitalRead(6) == 1) && (digitalRead(7) == 0)){
+    buzzer_sound_mode1(DUTY);
+  }else if((digitalRead(6) == 1) && (digitalRead(7) == 1)){
+    buzzer_sound_mode2(DUTY);
+  }else if((digitalRead(6) == 0) && (digitalRead(7) == 0)){
+    DDRD &= ~(1<<BUZZER);
+  }
+}
---
+++ CWS_for_sensor_board/CWS_for_sensor_board.ino
@@ -0,0 +1,220 @@
+/* Title : Collision Warning Systems(CWS) for personal mobiltiy */
+/* >>> For sensor board */
+
+/* Constant for setup */
+#define trigPin 12 //ultrasonic output
+#define echoPin 13 //ultrasonic input
+#define ENCODER 2 //encoder input - PD2
+#define BUZZER 3 // buzzer output - PD3
+#define bit1ForBuzzer 4
+#define bit2ForBuzzer 5
+
+/* Ultrasonic code */
+//Warning : 둘 다 실수로 나오므로 Serial.print로 확인하기 위해서는 강제로 형변환 필요 - Serial.print(String(float_value));
+#define offsetForZero 13 //초음파센서 영점을 맞추기 위한 변수
+#define delayTime 50 //초음파센서, 엔코더 딜레이
+volatile float sensorValues[20]; //필터링을 위한 배열 선언
+
+/* 초음파 센서로 전방거리를 측정하는 함수 */
+float measure_distance(){
+  for(int i = 0 ; i < 20-1 ; i++){
+    sensorValues[i]= sensorValues[i+1];
+  }
+  digitalWrite(echoPin,LOW);
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+
+  unsigned long duration1 = pulseIn(echoPin,HIGH);
+  float distance = ((float) (340*duration1)/10000)/2;
+
+  sensorValues[20-1] = distance;
+  float filterDistance = 0;
+  for (int i=0; i< 20; i++){
+    filterDistance += sensorValues[i];
+  }
+  filterDistance /=20;
+  return filterDistance;
+}
+
+ /* 초음파 센서로 상대속도를 측정,계산하는 함수 */
+float calculate_relative_velocity_using_Ultrasonic(){
+  float distance1, distance2, velocity, filtered_velocity;
+  unsigned long duration1, duration2;
+
+  digitalWrite(echoPin,LOW);
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+
+  duration1 = pulseIn(echoPin,HIGH);
+  distance1 = ((float) (340*duration1)/10000)/2;
+
+  delay(delayTime);
+  digitalWrite(echoPin,LOW);
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+
+  duration2 = pulseIn(echoPin,HIGH);
+  distance2 = ((float) (340*duration2)/10000)/2; //340m/s * (1s / 10^6ms) * duaration ms * 100 cm / 1m
+
+  velocity = ((float)(distance2-distance1)/delayTime*0.001) - offsetForZero;
+
+  /* Low pass filter */
+  for(int i=0 ; i < 100 ; i++){ //0.1초에 대한 평균값
+    filtered_velocity += velocity;
+    delayMicroseconds(100);
+  }
+  filtered_velocity /= 100;
+
+  return filtered_velocity;
+}
+
+
+
+/* Encoder code */
+//초를 표시하기 위한 코드 : Serial.print(timeCurr / 1000);
+
+//For setup() setting for encoder
+volatile int encoder = 0;
+
+void ISR_encoder(){
+  encoder++;
+}
+
+float measure_velocity_using_encoder(){
+  #define wheelDiameter  6.6 // 바퀴 지름 (cm)
+  #define rotationPerRevolution  130
+  #define distancePerRevolution  2 * PI * (wheelDiameter / 2) //한 바퀴당 이동거리(cm) 
+  unsigned long timePrev = 0;
+  unsigned long timeCurr = 0;
+  volatile int encoderPrev = 0;
+  volatile float revolutions = 0;
+  volatile float rpm = 0;
+  volatile float velocity = 0;
+  volatile float filteredVelocity = 0;
+  
+  timeCurr = millis();
+  if(timeCurr - timePrev > delayTime){ //1초마다 출력
+    timePrev = timeCurr;
+    noInterrupts();
+
+    revolutions = float(encoder-encoderPrev)/rotationPerRevolution;
+    rpm = revolutions * 60; //초당 회전수 1초 : (timeCurr - timePrev)
+
+    // RPM을 cm/s로 변환
+    velocity = (rpm * distancePerRevolution) / 60; // cm/s로 변환
+    
+    encoderPrev = encoder;
+    interrupts();
+
+    return velocity;
+  }
+}
+
+
+/* Buzzer code */
+#define no_buze 990
+#define buze_with_partial_break 991
+#define buze_eve_full_break 992
+
+void make_warning_sound(){
+  volatile int situation = no_buze; //충돌예상거리에 따른 상황분류를 위한 변수
+  volatile float boundary_for_partial_break = 30; /////////////////////////////나중에 엔코더 속도 맞춰서 수식 써야하는 부분//////
+  volatile float boundary_for_full_break = 10;////////////////////////////////////////////////////////////////////////
+
+  if(measure_distance() < boundary_for_full_break){
+    situation = buze_eve_full_break;
+  }else if(measure_distance() < boundary_for_partial_break){
+    situation = buze_with_partial_break;
+  }else{
+    situation = no_buze;
+  }
+
+  if(situation == buze_with_partial_break){
+    digitalWrite(bit1ForBuzzer,HIGH); //With partial breaking
+    digitalWrite(bit2ForBuzzer,LOW); //With partial breaking
+  }else if(situation == buze_eve_full_break){
+    digitalWrite(bit1ForBuzzer,HIGH); //With partial breaking
+    digitalWrite(bit2ForBuzzer,HIGH); //With partial breaking
+  }else{
+    digitalWrite(bit1ForBuzzer,LOW); //With partial breaking
+    digitalWrite(bit2ForBuzzer,LOW); //With partial breaking 
+  }
+}
+
+void setup(){
+  /* Ultrasonic */
+  pinMode(echoPin,INPUT);
+  pinMode(trigPin,OUTPUT);
+
+  /* Encoder */
+  Serial.begin(57600);
+  pinMode(ENCODER, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENCODER), ISR_encoder, FALLING);
+
+  /* Buzzer */
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+}
+
+void loop(){
+  /* Test for ultra-sonic */
+  /*
+  DDRD &= ~(1<<BUZZER);
+  float value = measure_distance();
+  Serial.print(String(value));
+  Serial.println();
+  delay(200);
+  */
+
+  /* Test for encoder */
+  float value = measure_distance();
+  Serial.print("거리:");
+  Serial.print(String(value));
+  Serial.print(",");
+  make_warning_sound();
+  Serial.print(digitalRead(4));
+  Serial.print(digitalRead(5));
+  Serial.println();
+  //delay(50);
+}
+
+/* Sound Sensor
+#define SOUND A0
+int vol = 0;
+int count = 0;
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  vol = analogRead(SOUND);
+
+  if(vol>300){
+    count++;
+    Serial.print("sound: ");
+    Serial.println(count);
+    Serial.print("volume: ");
+    Serial.println(vol);
+    delay(500);
+  }
+
+  /*
+  Serial.print("sound volume : ");
+  Serial.print(vol);
+  Serial.println();
+  delay(100);
+  */
+
+
+/* 바퀴 지름 6.6 */
